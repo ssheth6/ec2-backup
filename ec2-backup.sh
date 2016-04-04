885807f0a3ac1386324e287fd01154e46104cfd6
skarinat <smruthi23@gmail.com>
2016-04-04 15:03:27 -0400

    options

--- ec2-backup.sh
+++ ec2-backup.sh
@@ -1,1 +1,35 @@
 #!/bin/bash
+
+##
+##
+## Main
+##
+##
+
+
+  option=$1
+  inet_string=""
+  usage=$0"-h|-m|-v"
+  export IFS=
+  case "$option" in
+
+      "-h")
+	  helptext
+	  ;;
+
+      "-m")
+          method_type    #call method function
+	  ;;
+
+      "-v")
+	  volume_id  #call volumeID function
+	  ;;
+      *)
+        echo " The parameter passed is invalid" $usage
+        
+	;;
+  esac
+
+###end of case statement
+
+exit
