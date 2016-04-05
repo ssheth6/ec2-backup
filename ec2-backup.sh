54fbed19df5bcd559b354d314eaf50fb238572d2
Ramit Farwaha <rfarwaha@sailthru.com>
2016-04-05 19:38:08 -0400

    add $check for size of volume to create

--- ec2-backup.sh
+++ ec2-backup.sh
@@ -9,7 +9,7 @@ timeZone= aws ec2 describe-instances | grep AvailabilityZone | awk '{print $2}' 
 createKeyPair= aws ec2 create-key-pair --key-name ec2BackUpKeyPair --output text > ~/ec2BackUpKeyPair.pem | chmod 600 ~/ec2BackUpKeyPair.pem
 runInstance= aws ec2 run-instances --instance-type t1.micro --key ec2BackUpKeyPair --image-id ami-c27e48aa
 
-createVolume= aws ec2 create-volume --size 1 --availability-zone $timeZone --volume-type standard
+createVolume= aws ec2 create-volume --size $CHECK --availability-zone $timeZone --volume-type standard
 attachVolume= aws ec2 attach-volume --volume-id $volume --instance-id $instanceId --device /dev/sdf
 
 volume= aws ec2 describe-volumes | grep VolumeId volume.txt | awk '{print $2}' | sed 's/\"//g' | sed 's/\,//g'
