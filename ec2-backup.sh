a68678b537e0a8d8e77e4e2848308b4e02255a3d
Sneha Sheth <ssheth6@stevens.edu>
2016-04-07 00:48:08 -0400

    variables getting invoked automatically without calling so used full commands instead of variables

--- ec2-backup.sh
+++ ec2-backup.sh
@@ -2,25 +2,25 @@
 
 ##
 ###Begin Variables###
-publicDns= aws ec2 describe-instances | grep PublicDns | head -1 | awk '{print $2}' | sed 's/\"//g' | sed 's/\,//g'
-instanceId= aws ec2 describe-instances | grep InstanceId | head -1 | awk '{print $2}' | sed 's/\"//g' | sed 's/\,//g'
-timeZone= aws ec2 describe-instances | grep AvailabilityZone | awk '{print $2}' | sed 's/\"//g'
+#publicDns= aws ec2 describe-instances | grep PublicDns | head -1 | awk '{print $2}' | sed 's/\"//g' | sed 's/\,//g'
+#instanceId= aws ec2 describe-instances | grep InstanceId | head -1 | awk '{print $2}' | sed 's/\"//g' | sed 's/\,//g'
+#timeZone= aws ec2 describe-instances | grep AvailabilityZone | awk '{print $2}' | sed 's/\"//g'
 
-createKeyPair= aws ec2 create-key-pair --key-name ec2BackUpKeyPair --output text > ~/ec2BackUpKeyPair.pem | chmod 600 ~/ec2BackUpKeyPair.pem
-runInstance= aws ec2 run-instances --instance-type t1.micro --key ec2BackUpKeyPair --image-id ami-c27e48aa
+#createKeyPair= aws ec2 create-key-pair --key-name ec2BackUpKeyPair --output text > ~/ec2BackUpKeyPair.pem | chmod 600 ~/ec2BackUpKeyPair.pem
+#runInstance= aws ec2 run-instances --instance-type t1.micro --key ec2BackUpKeyPair --image-id ami-c27e48aa
 
-createVolume= aws ec2 create-volume --size $CHECK --availability-zone $timeZone --volume-type standard
-attachVolume= aws ec2 attach-volume --volume-id $volume --instance-id $instanceId --device /dev/sdf
+#createVolume= aws ec2 create-volume --size $CHECK --availability-zone $timeZone --volume-type standard
+#attachVolume= aws ec2 attach-volume --volume-id $volume --instance-id $instanceId --device /dev/sdf
 
-volume= aws ec2 describe-volumes | grep VolumeId | awk '{print $2}' | sed 's/\"//g' | sed 's/\,//g'
+#volume= aws ec2 describe-volumes | grep VolumeId | awk '{print $2}' | sed 's/\"//g' | sed 's/\,//g'
 
-volumeId= aws ec2 describe-volumes | grep VolumeId | head -1 | awk '{print $2}' | sed 's/\"//g' | sed 's/\,//g'
+#volumeId= aws ec2 describe-volumes | grep VolumeId | head -1 | awk '{print $2}' | sed 's/\"//g' | sed 's/\,//g'
 
 #mount_dir= ssh ec2-user@$publicDns 'sudo su file -s /dev/sdf | mkfs -t ext4 /dev/sdf | mkdir /$dir | mount /dev/sdf /$dir'
 
-mountVolume= ssh -i ec2BackUpKeyPair.pem ec2-user@$publicDns 'sudo file -s /dev/sdf | sudo mkfs -t ext4 /dev/sdf | sudo mkdir /data | sudo mount /dev/sdf /data'
+#mountVolume= ssh -i ec2BackUpKeyPair.pem ec2-user@$publicDns 'sudo file -s /dev/sdf | sudo mkfs -t ext4 /dev/sdf | sudo mkdir /data | sudo mount /dev/sdf /data'
 
-volumeState= aws ec2 describe-volumes | grep State | head -1 | awk '{print $2}' | sed 's/\"//g' | sed 's/\,//g'
+#volumeState= aws ec2 describe-volumes | grep State | head -1 | awk '{print $2}' | sed 's/\"//g' | sed 's/\,//g'
 ####End Variables ###
 ##
 
@@ -29,16 +29,22 @@ volumeState= aws ec2 describe-volumes | grep State | head -1 | awk '{print $2}' 
 ##
 
 generateKeyPair() {
+	if [ -f ~/ec2BackUpKeyPair.pem ]; then
+		echo "Key pair already exists"
+	else
 
-        $createKeyPair
+        	aws ec2 create-key-pair --key-name ec2BackUpKeyPair --output text > ~/ec2BackUpKeyPair.pem | chmod 600 ~/ec2BackUpKeyPair.pem
+		echo "Key pair created"
+	fi
 }
 
 runInstance() {
 
-	$runInstance
-	$publicDns
-	$instanceId
-	$timeZone
+	runInstance= aws ec2 run-instances --instance-type t1.micro --key ec2BackUpKeyPair --image-id ami-c27e48aa
+	publicDns= aws ec2 describe-instances | grep PublicDns | head -1 | awk '{print $2}' | sed 's/\"//g' | sed 's/\,//g'
+	echo "public DNS found"
+	instanceId= aws ec2 describe-instances | grep InstanceId | head -1 | awk '{print $2}' | sed 's/\"//g' | sed 's/\,//g'
+	timeZone= aws ec2 describe-instances | grep AvailabilityZone | awk '{print $2}' | sed 's/\"//g'
 }
 
 createVolume() {
@@ -56,34 +62,33 @@ createVolume() {
 	
 	##If volume flag value is empty we create a ne wone and attach
 	if [$v == ' ' ]; then
-		$createVolume
-		$volumeId
+		createVolume= aws ec2 create-volume --size $CHECK --availability-zone $timeZone --volume-type standard
+		volumeId= aws ec2 describe-volumes | grep VolumeId | awk '{print $2}' | sed 's/\"//g' | sed 's/\,//g'
 		echo $volumeId
-		$attachVolume
-		$mountVolume
+		attachVolume= aws ec2 attach-volume --volume-id $volume --instance-id $instanceId --device /dev/sdf
+		mountVolume= ssh -i ec2BackUpKeyPair.pem ec2-user@$publicDns 'sudo file -s /dev/sdf | sudo mkfs -t ext4 /dev/sdf | sudo mkdir /data | sudo mount /dev/sdf /data'
 	
 	##If volumen flag has a value, check if it is already attached. If so, echo an error and if not use that volume id to attach and mount
 	else
-        	$volumeState
+        	volumeState= aws ec2 describe-volumes | grep State | head -1 | awk '{print $2}' | sed 's/\"//g' | sed 's/\,//g'
 		
 		if [ volumeState == 'attached' ]; then
 			echo "Please specify a volume that is available."
 		else
-			$attachVolume
-                	$mountVolume
+			attachVolume= aws ec2 attach-volume --volume-id $vol --instance-id $instanceId --device /dev/sdf
+                	mountVolume= ssh -i ec2BackUpKeyPair.pem ec2-user@$publicDns 'sudo file -s /dev/sdf | sudo mkfs -t ext4 /dev/sdf | sudo mkdir /data | sudo mount /dev/sdf /data'
 		fi
 	fi
 }
 
-backupType()
+createBackup()
 {
         if [ $m == 'rysnc' ];
                 then
-
-                rsync -az $dir ec2-user@$publicDns:/dev/sdf
+                rsync -az $dir ec2-user@$publicDns:/data
         elif [ $m == 'dd' ];
 		then
-                dd if=$dir of=$publicDns:/dev/sdf bs=$CHECK
+                dd if=$dir of=$publicDns:/data bs=$CHECK
 	else
 		echo "Please specify a valid value. Available methods are 'rsync' and 'dd'"
         fi
@@ -94,13 +99,18 @@ backupType()
 ## Main
 ##
 ##
-  while getopts ":h:m:v:" o; do
+  while getopts ":hm:v:" o; do
     case "${o}" in
         m)
             m=${OPTARG}
             dir=$3
-                #echo "$m"
-                #echo "$dir"
+               # echo $m
+               # echo $dir
+	#	generateKeyPair
+	#	runInstance
+	#	createVolume
+	#	createBackup
+		
             ;;
         v)
             v=${OPTARG}
