2ee2309eccb0687d0b3ebeb99f3d2f662131ee60
Ramit Farwaha <rfarwaha@sailthru.com>
2016-04-11 11:25:40 -0400

    add in comments

--- ec2-backup.sh
+++ ec2-backup.sh
@@ -1,4 +1,11 @@
-#!/bin/bash
+#!/bin/bash 
+#title			:ec2-backup.sh
+#description	:This script will back up a local directory to an AWS EC2 Volume
+#authors		:Sneha Sheth, Smruthi Karinat, Ramit Farwaha
+#date			:April 11, 2016 
+#version		:0.1
+#bash_version	:4.2.25(1)-release
+#=======================================================================================
 
 ##
 ##FUNCTIONS
@@ -6,6 +13,7 @@
 
 flags_aws=''
 flags_ssh=''
+#Print out parameter if verbose eq true
 EC2_BACKUP_VERBOSE='false'
 
 generateKeyPair() {
@@ -21,23 +29,26 @@ generateKeyPair() {
                         exit 1
 		else
 			groupName="ec2-backup"
-			checkGroup=$(aws ec2 describe-security-groups --group-names $groupName | grep GroupName | awk '{print $2}' | sed 's/\"//g' | sed 's/\,//g') 1>/dev/null 2>/dev/null	
+			checkGroup=$(aws ec2 describe-security-groups --group-names $groupName | grep GroupName | awk '{print $2}' | sed 's/\"//g' | sed 's/\,//g') 1>/dev/null 2>/dev/null
+				[ $EC2_BACKUP_VERBOSE = 'true' ] && echo "Your Group Name is $groupName"	
 			if [ "$checkGroup" == "" ];
 			then
+				[ $EC2_BACKUP_VERBOSE = 'true' ] && echo "Currently checking Security Group..."
 				groupId=$(aws ec2 create-security-group --group-name $groupName --description "EC2 backup tool group" | grep GroupId | head -1 | awk '{print $2}' | sed 's/\"//g')
                         	tmp=$(aws ec2 authorize-security-group-ingress --group-name $groupName --protocol tcp --port 22 --cidr 0.0.0.0/0)
                 	fi
 		fi
-	else			       
+	else
+	# Check if a key pair already exisit and use the existing instead of creating a new on the fly			       
 		if [ -f ec2BackUpKeyPair ]; then
 			[ $EC2_BACKUP_VERBOSE = 'true' ] && echo "Key pair already exists"
 			groupName="ec2-backup-sg"
 			key_path="ec2BackUpKeyPair"
                         key_name="ec2BackUpKeyPair"
 		else
-				# Generate a Key Pair and output to users directory where script is executed
-				# Generate a Security Group and authorize all IPs via port 22
-				# This code ignores the vulnerability of having all IPs able to connect to port 22 
+			# Generate a Key Pair and output to users directory where script is executed
+			# Generate a Security Group and authorize all IPs via port 22
+			# This code ignores the vulnerability of having all IPs able to connect to port 22 
 			groupName="ec2-backup-sg"
         		aws ec2 create-key-pair --key-name ec2BackUpKeyPair --query 'KeyMaterial' --output text > ec2BackUpKeyPair
 			groupId=$(aws ec2 create-security-group --group-name $groupName --description "EC2 backup tool group" | grep GroupId | head -1 | awk '{print $2}' | sed 's/\"//g')
@@ -59,13 +70,15 @@ runInstance() {
 	[ $EC2_BACKUP_VERBOSE = 'true' ] &&  echo "`echo "$EC2_BACKUP_FLAGS_AWS"`"
         if [ -n "`echo "$EC2_BACKUP_FLAGS_AWS"`" ]
         	then
-                	flags_aws="`echo $EC2_BACKUP_FLAGS_AWS`"
-			groupId=$(aws ec2 describe-security-groups --group-names $groupName | grep GroupId | head -1 | awk '{print $2}' | sed 's/\"//g')
+                flags_aws="`echo $EC2_BACKUP_FLAGS_AWS`"
+				groupId=$(aws ec2 describe-security-groups --group-names $groupName | grep GroupId | head -1 | awk '{print $2}' | sed 's/\"//g')
         		instanceId=$(aws ec2 run-instances $flags_aws --key $key_name --image-id ami-d9dd0eb0 --security-group-ids $groupId | grep InstanceId | head -1 | awk '{print $2}' | sed 's/\"//g' | sed 's/\,//g')
+        			[ $EC2_BACKUP_VERBOSE = 'true' ] &&  echo "$flags_aws"
         else
-                	flags_aws="--instance-type t2.micro"
-			groupId=$(aws ec2 describe-security-groups --group-names $groupName | grep GroupId | head -1 | awk '{print $2}' | sed 's/\"//g')
+                flags_aws="--instance-type t2.micro"
+				groupId=$(aws ec2 describe-security-groups --group-names $groupName | grep GroupId | head -1 | awk '{print $2}' | sed 's/\"//g')
         		instanceId=$(aws ec2 run-instances $flags_aws --key $key_name --image-id ami-fce3c696 --security-group-ids $groupId | grep InstanceId | head -1 | awk '{print $2}' | sed 's/\"//g' | sed 's/\,//g')
+        			[ $EC2_BACKUP_VERBOSE = 'true' ] &&  echo "$flags_aws"
         fi
 	
 		# Sleep is required here. Spinning up the Instance takes a bit of time to become visible.
@@ -92,17 +105,17 @@ createVolume() {
                 SIZE=$((2*$CHECK/1000))
         fi
 	
-	##If volume flag value is empty we create a new one and attach
-	if [ "$opt_v" == "" ]; then
-		volumeId=$(aws ec2 create-volume --size $SIZE --availability-zone $instanceZone --volume-type standard | grep VolumeId | awk '{print $2}' | sed 's/\"//g' | sed 's/\,//g')
-			echo "$volumeId"
-			# Sleep here is required as it takes a bit of time (1min) for the volume to become visible
-			[ $EC2_BACKUP_VERBOSE = 'true' ] && echo "Volume $volumeId created, please wait 1 min"
-			[ $EC2_BACKUP_VERBOSE = 'true' ] && echo "Waiting..."
-				sleep 60
+		# If volume flag value is empty we create a new one and attach
+		if [ "$opt_v" == "" ]; then
+			volumeId=$(aws ec2 create-volume --size $SIZE --availability-zone $instanceZone --volume-type standard | grep VolumeId | awk '{print $2}' | sed 's/\"//g' | sed 's/\,//g')
+				echo "$volumeId"
+				# Sleep here is required as it takes a bit of time (1min) for the volume to become visible
+				[ $EC2_BACKUP_VERBOSE = 'true' ] && echo "Volume $volumeId created, please wait 1 min"
+				[ $EC2_BACKUP_VERBOSE = 'true' ] && echo "Waiting..."
+					sleep 60
 
-		attachVolume=$(aws ec2 attach-volume --volume-id $volumeId --instance-id $instanceId --device /dev/sdf)
-			[ $EC2_BACKUP_VERBOSE = 'true' ] && echo "New Volume $volumeId has been attached"
+			attachVolume=$(aws ec2 attach-volume --volume-id $volumeId --instance-id $instanceId --device /dev/sdf)
+				[ $EC2_BACKUP_VERBOSE = 'true' ] && echo "New Volume $volumeId has been attached"
 	
 		# SSH on Remote Host
 		# Create the Filesystem
@@ -115,7 +128,7 @@ createVolume() {
 		df -h
 		exit
 EOF
-		
+		# Continue to feed usual information when verbose is called
 		[ $EC2_BACKUP_VERBOSE = 'true' ] &&  echo "Mounted Complete"
 	
 	#If volume flag has a value, check if it is already attached. If so, echo an error and if not use that volume id to attach and mount
@@ -167,6 +180,7 @@ createBackup()
 volumeID()
 {
 	generateKeyPair
+		[ $EC2_BACKUP_VERBOSE = 'true' ] &&  echo "A Key Pair is being generated" 
         runInstance
 
 	volumeState=$(aws ec2 describe-volumes --volume-ids $opt_v | grep State | head -1 | awk '{print $2}' | sed 's/\"//g' | sed 's/\,//g')
@@ -176,11 +190,15 @@ volumeID()
 	
 	if [ "$volumeState" == "attached" ]; then
 		echo "The volume $opt_v provided is attached"
+		#Complete - Shut down Instance
+		[ $EC2_BACKUP_VERBOSE = 'true' ] &&  echo "Instance is now Terminating"
 		terminateInstance
 		exit 1
 	
 	elif [ "$instanceZone" != "$volumeZone" ]; then
 		echo "The volume $opt_v is  not in the same availability zone as the instance"
+		#Conditional Err - Shut down Instance
+		[ $EC2_BACKUP_VERBOSE = 'true' ] &&  echo "Instance is now Terminating"
 		terminateInstance
 		exit 1
 	
@@ -200,7 +218,9 @@ volumeID()
             ssh -t -T -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -i "$key_path" ubuntu@$publicDns 1>/dev/null 2>/dev/null << EOF
             sudo mkfs -t ext4 /dev/xvdf 1>/dev/null 2>/dev/null
             sudo mkdir -m 755 /data
+            	[ $EC2_BACKUP_VERBOSE = 'true' ] &&  echo "Currently creating Directory to mount on Remote end"
             sudo mount /dev/xvdf /data
+            	[ $EC2_BACKUP_VERBOSE = 'true' ] &&  echo "Currently mounting...Please Wait"
             df -h
             exit
 EOF
@@ -212,6 +232,10 @@ EOF
 	
 }
 
+# We need to terminate instance for 3 situations:
+# 1) User runs script and hits 'cntrl+C'
+# 2) Script finishes and terminating instance (NOT Volume) is needed
+# 3) Call termination where 'exit' is being called as the script will 'exit'
 terminateInstance()
 {
 	if [ "$instanceId" == "" ]; then 
@@ -223,13 +247,14 @@ terminateInstance()
 	fi 
 }
 
-
+# Terminate Instance when end-user hits cntrl+c during running script
 function ctrl_c() {
   
       terminateInstance
 	exit 1
 }
 
+# Print Help function page
 displayHelp()
 {
 	echo "
@@ -258,6 +283,8 @@ opt_m=""
 opt_v=""
 trap ctrl_c INT
 
+# As long as the below is greater than 0, execute the following case statements
+# using shift for corner cases when -m/-v is given blank without a valid parameter
 while [ $# -gt 0 ] 
 do
 	case $1 in
@@ -289,6 +316,9 @@ do
 	esac
 done
 
+# Statements for -m and -v calling
+# Display If Statements if -m/-v has/doesn't have parameter
+# Call IF for each possible situation
    if [[ "$opt_m" == "" && "$opt_v" != "" ]]; then
 	opt_m="dd"
 	volumeID
